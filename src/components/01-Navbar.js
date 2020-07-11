9da9d201c6ac7776bd862058e0c42d8ee33c8613
Caitlin <chiangcaitlin2003@gmail.com>
2020-07-11 22:41:00 +0800

    Homepage, Aboutpage, Footer

--- src/components/01-Navbar.js
+++ src/components/01-Navbar.js
@@ -1,47 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react'
+import { HashLink as Link } from 'react-router-hash-link'
 
 
-function Navbar() {
-	return (
-		<header>
-		
-			<section class="desktopNavbar">
-				<div class="logo"> <img src="/images/Soren_Logo.png" /> </div>
-				<ul>
-					<li> <Link to="/">        <a href="#home">Home</a>          </Link> </li>
-					<li> <Link to="/">        <a href="#about">About</a>        </Link> </li>
-					<li> <Link to="/shop">    <a href="#shop">Shop</a>          </Link> </li>
-					<li> <Link to="/contact"> <a href="#contact">Contact Us</a> </Link> </li>
-				</ul>
-				<div class="cart"> <img src="/images/ShoppingCart.png" /> </div>
-			</section>
+class Navbar extends Component {
+	state = {}
 
-			<section class="mobileNavbar">
-				<div class="logo"> <img src="/images/Soren_Logo.png" /> </div>
-				<div class="menu-wrap">
-					<input type="checkbox" class="toggler" />
+	render() {
+		return (
+			<header>
+			
+				<section class="desktopNavbar">
+					<div class="logo"> <img src="/images/Soren_Logo.png" /> </div>
+					<ul>
+						<li> <Link to="/#home">   <a href="#">Home</a>       </Link> </li>
+						<li> <Link to="/#about">  <a href="#">About</a>      </Link> </li>
+						<li> <Link to="/shop">    <a href="#">Shop</a>       </Link> </li>
+						<li> <Link to="/contact"> <a href="#">Contact Us</a> </Link> </li>
+					</ul>
+					<div class="cart"> <img src="/images/ShoppingCart.png" /> </div>
+				</section>
 
-					<div class="hamburger"><div></div></div>
+				<section class="mobileNavbar">
+					<div class="logo"> <img src="/images/Soren_Logo.png" /> </div>
+					<div class="menu-wrap">
+						<input type="checkbox" class="toggler" />
 
-					<div class="menu">
-						<div>
+						<div class="hamburger"><div></div></div>
+
+						<div class="menu">
 							<div>
-								<ul>
-									<li> <Link to="/">        <a href="#home">Home</a>          </Link> </li>
-									<li> <Link to="/">        <a href="#about">About</a>        </Link> </li>
-									<li> <Link to="/shop">    <a href="#shop">Shop</a>          </Link> </li>
-									<li> <Link to="/contact"> <a href="#contact">Contact Us</a> </Link> </li>
-								</ul>
+								<div>
+									<ul>
+										<li> <Link to="/#home">   <a href="#">Home</a>       </Link> </li>
+										<li> <Link to="/#about">  <a href="#">About</a>      </Link> </li>
+										<li> <Link to="/shop">    <a href="#">Shop</a>       </Link> </li>
+										<li> <Link to="/contact"> <a href="#">Contact Us</a> </Link> </li>
+									</ul>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div class="cart"> <img src="/images/ShoppingCart.png" /> </div>
-			</section>
+					<div class="cart"> <img src="/images/ShoppingCart.png" /> </div>
+				</section>
 
-		</header>
-	)
+			</header>
+		)
+	}
 }
 
 export default Navbar
--- src/components/02-Homepage.js
+++ src/components/02-Homepage.js
@@ -9,15 +9,28 @@ function Homepage() {
 			<section id="home">
 				<div class="mainLogo"> <img src="/images/Soren_Logo.png" /> </div>
 
-				<div class="description">
+				<div class="desktopTitlePage">
 					<h1>Quality. Style. Elegance.</h1>
+				</div>
+
+				<div class="mobileTitlePage">
+					<h1>Quality.</h1>
+					<h1>Style.</h1>
+					<h1>Elegance.</h1>
 				</div>
 			</section>
 
 			<section id="about">
+				<div class="aboutTitle">
+					<h1>ABOUT SOREN</h1>
+				</div>
 
+				<div class="aboutDescription">
+					<p>Here at Soren, we grant people with a means to express their individuality, through providing high-quality clothing at a reasonable price.</p>
+				</div>
 			</section>
 
+			<footer>&#169; 2020 by Soren.</footer>
 		</div>
 	)
 }
